aee3d53a4140c12bafb09f4f524bfc9704accd68
dhanushramudri <ramudridhanush@gmail.com>
2024-03-15 21:09:26 +0530

    Added HomePage component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <div className="App">
       In this i want to practice all the mongodb functions
+      <HomePage />
     </div>
   );
 }
